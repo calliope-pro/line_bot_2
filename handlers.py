2618d49ed5dac4323f9247c6f8c523e2c8080647
calliope <calliope183@gmail.com>
2022-09-07 03:50:55 +0900

    atomize method

--- handlers.py
+++ handlers.py
@@ -3,7 +3,7 @@ from typing import List
 
 from deta import _Base, _Drive
 from linebot import AsyncLineBotApi
-from linebot.models.events import Event, MessageEvent, ImageMessage
+from linebot.models.events import Event, MessageEvent, FollowEvent
 from linebot.models.send_messages import TextSendMessage
 
 from settings import BASE_PROJECT_URL
@@ -14,10 +14,38 @@ class EventsHandler:
         self.events = events
         self.db = db
         self.drive = drive
-        print(line_bot_api)
 
-    # async def handle_message_event(self):
-        
+    async def handle_message_event(self, event: MessageEvent):
+        if event.message.type == 'image':
+            data = await self.line_bot_api.get_message_content(event.message.id)
+            binary_data = b''
+            async for b in data.iter_content():
+                binary_data += b
+            await data.response.close()
+            file_name = self.drive.put(
+                name=f'{event.message.id}.png',
+                data=binary_data,
+                content_type=data.content_type,
+            )
+            await self.line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'{BASE_PROJECT_URL}/images/{file_name}\nに保存しました')
+            )
+        else:
+            await self.line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    # ImageSendMessage(
+                    #     f'{BASE_PROJECT_URL}/images/14734080206120.png/',
+                    #     f'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=',
+                    # ),
+                    TextSendMessage(text=f'You: {event.message.text}'),
+                    TextSendMessage(text="\n".join(map(lambda x: f'{BASE_PROJECT_URL}/images/{x}', self.drive.list()["names"]))),
+                ]
+            )
+
+    async def handle_follow_event(self, event: FollowEvent):
+        pass
 
     async def handler(self):
         # ベント処理List[Event]
@@ -25,34 +53,14 @@ class EventsHandler:
             try:
                 assert event.source.user_id == os.environ['MY_LINE_USER_ID'], 'user_idが異なります'
 
-                if event.message.type == 'image':
-                    data = await self.line_bot_api.get_message_content(event.message.id)
-                    binary_data = b''
-                    async for b in data.iter_content():
-                        binary_data += b
-                    await data.response.close()
-                    file_name = self.drive.put(
-                        name=f'{event.message.id}.png',
-                        data=binary_data,
-                        content_type=data.content_type,
-                    )
-                    await self.line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=f'{BASE_PROJECT_URL}/images/{file_name}\nに保存しました')
-                    )
-
-                else:
-                    await self.line_bot_api.reply_message(
-                        event.reply_token,
-                        [
-                            # ImageSendMessage(
-                            #     f'{BASE_PROJECT_URL}/images/14734080206120.png/',
-                            #     f'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=',
-                            # ),
-                            TextSendMessage(text=f'You: {event.message.text}'),
-                            TextSendMessage(text="\n".join(map(lambda x: f'{BASE_PROJECT_URL}/images/{x}', self.drive.list()["names"]))),
-                        ]
-                    )
+                # メッセージイベント
+                if isinstance(event, MessageEvent):
+                    await self.handle_message_event(event)
+                    break
+                # 友達追加イベント
+                elif isinstance(event, FollowEvent):
+                    await self.handle_follow_event(event)
+                    break
 
             except AssertionError as e:
                 print(e)
