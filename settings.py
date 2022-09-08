22fe5ef45145fe16e4c966b2f6d34ef6d8bd9a0a
calliope <calliope183@gmail.com>
2022-09-09 00:44:37 +0900

    fix richmenu

--- settings.py
+++ settings.py
@@ -3,10 +3,13 @@ import os
 
 from deta import Deta
 from aiohttp import ClientSession
-from deta import App
 from fastapi import FastAPI
 from linebot import AsyncLineBotApi, WebhookParser
 from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
+
+if os.environ.get('DETA_RUNTIME', 'false') == 'true':
+    from deta import App
+    app = App(FastAPI())
 
 BASE_PROJECT_URL = 'https://calliope-bot.deta.dev'
 
@@ -16,18 +19,21 @@ CHANNEL_ACCESS_TOKEN = os.environ['CHANNEL_ACCESS_TOKEN']
 LINE_BOT_API = AsyncLineBotApi(CHANNEL_ACCESS_TOKEN, AiohttpAsyncHttpClient(ClientSession()))
 LINE_PARSER = WebhookParser(CHANNEL_SECRET)
 
-app = App(FastAPI())
-
 deta = Deta(os.environ['DETA_PROJECT_KEY'])
 
 DB_LINE_ACCOUNTS = deta.Base('line_accounts')
 DB_SCRAPE_RESULTS = deta.Base('scrape_results')
 DRIVE_LINE_BOT_DRIVE = deta.Drive('line-bot-drive')
 
-IS_MAINTENANCE = bool(int(os.environ.get('IS_MAINTENANCE', 1)))
+IS_MAINTENANCE = bool(int(os.environ['IS_MAINTENANCE']))
 
-class Mode(Enum):
+class PostbackActionData(Enum):
     normal = 'normal'
+    memo = 'memo'
     memo_post = 'memo_post'
     memo_deletion = 'memo_deletion'
-    remider_registration = 'reminder_registration'
+    reminder = 'reminder'
+    reminder_registration = 'reminder_registration'
+    usage = 'usage'
+    inquiry = 'inquiry'
+    terminate = 'terminate'
