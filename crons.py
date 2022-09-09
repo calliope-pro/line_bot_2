76b5864640d768ba0cf2d458cf608f6063ce5fd1
calliope <calliope183@gmail.com>
2022-09-09 23:33:05 +0900

    test cron

--- crons.py
+++ crons.py
@@ -1,21 +1,17 @@
-import asyncio
 from datetime import datetime
-from typing import List
 
 import requests
-from linebot.models.send_messages import TextSendMessage
-from pydantic import parse_obj_as
 
-from models import ReminderWithKeyModel
-from settings import DB_REMINDERS, JST, LINE_BOT_API, app, BASE_PROJECT_URL
+from settings import JST, app, BASE_PROJECT_URL
 
 @app.lib.cron()
 def notify_works(event):
     now = datetime.now(JST).replace(tzinfo=None)
     if now.minute == 0 and now.hour % 4 == 2:
-        response = requests.get(f'{BASE_PROJECT_URL}/notify/')
-        print(response.status_code)
-        return
+        pass
+    response = requests.get(f'{BASE_PROJECT_URL}/notify/')
+    print(response.status_code)
+    return
 
 @app.lib.cron()
 def notify_reminders(event):
