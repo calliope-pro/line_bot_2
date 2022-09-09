11c29c5bd32e4fe08f593e4309060ffd8e53e4d4
calliope <calliope183@gmail.com>
2022-09-09 23:57:02 +0900

    just one cron

--- crons.py
+++ crons.py
@@ -5,16 +5,16 @@ import requests
 from settings import JST, app, BASE_PROJECT_URL
 
 @app.lib.cron()
-def notify_works(event):
-    now = datetime.now(JST).replace(tzinfo=None)
-    if now.minute == 0 and now.hour % 4 == 2:
-        pass
-    response = requests.get(f'{BASE_PROJECT_URL}/notify/')
-    print(response.status_code)
-    return
-
-@app.lib.cron()
 def notify_reminders(event):
-    response = requests.get(f'{BASE_PROJECT_URL}/remind/')
-    print(response.status_code)
-    return
+    try:
+        requests.get(f'{BASE_PROJECT_URL}/remind/')
+    except Exception as e:
+        print(e)
+
+    try:
+        now = datetime.now(JST).replace(tzinfo=None)
+        if now.minute == 0 and now.hour % 4 == 2:
+            pass
+        requests.get(f'{BASE_PROJECT_URL}/notify/')
+    except Exception as e:
+        print(e)
