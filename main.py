38208f1d7637c30482fd7bf05e58787e5323af60
calliope <calliope183@gmail.com>
2022-09-14 22:50:12 +0900

    add log

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ async def notify_reminders():
 async def show_image(file_name: str, token: str):
     user = UserWithKeyModel.parse_obj(DB_LINE_ACCOUNTS.fetch({"token": token}).items[0])
     image = DRIVE_LINE_BOT_DRIVE.get(f"{user.key}/{file_name}")
+    print(mimetypes.guess_type(file_name))
     return responses.StreamingResponse(
         image.iter_chunks(), media_type=mimetypes.guess_type(file_name)[0]
     )
