1bc988c00b8360b22adc7188a0559e8dbaf524c3
calliope <calliope183@gmail.com>
2022-09-14 18:38:30 +0900

    make new rich_menu

--- create_rich_menu.py
+++ create_rich_menu.py
@@ -1,7 +1,7 @@
 import os
 
 from linebot import LineBotApi
-from linebot.models.actions import PostbackAction
+from linebot.models.actions import PostbackAction, URIAction
 from linebot.models.rich_menu import (
     RichMenu,
     RichMenuArea,
@@ -18,30 +18,21 @@ CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 
 LINE_BOT_API = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
+rich_menu_width = 2500
+rich_menu_height = 1686
+
 raw_rich_menu = RichMenu(
     size=RichMenuSize(width=2500, height=1686),
     selected=True,
-    name="Main, 4divs, basic function",
+    name="Main, 6divs, basic function",
     chat_bar_text="See more!",
     areas=[
         RichMenuArea(
             bounds=RichMenuBounds(
-                x=250,
-                y=168,
-                width=750,
-                height=508,
-            ),
-            action=PostbackAction(
-                label="usage",
-                data=PostbackActionData.usage.value,
-            ),
-        ),
-        RichMenuArea(
-            bounds=RichMenuBounds(
-                x=1500,
-                y=168,
-                width=750,
-                height=508,
+                x=rich_menu_width*1/30,
+                y=0,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
             ),
             action=PostbackAction(
                 label="handle memos",
@@ -50,10 +41,10 @@ raw_rich_menu = RichMenu(
         ),
         RichMenuArea(
             bounds=RichMenuBounds(
-                x=250,
-                y=1011,
-                width=750,
-                height=508,
+                x=rich_menu_width*11/30,
+                y=0,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
             ),
             action=PostbackAction(
                 label="handle reminders",
@@ -62,10 +53,46 @@ raw_rich_menu = RichMenu(
         ),
         RichMenuArea(
             bounds=RichMenuBounds(
-                x=1500,
-                y=1011,
-                width=750,
-                height=508,
+                x=rich_menu_width*21/30,
+                y=0,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
+            ),
+            action=PostbackAction(
+                label="handle files",
+                data=PostbackActionData.file.value,
+            ),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(
+                x=rich_menu_width*1/30,
+                y=rich_menu_height*1/2,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
+            ),
+            action=PostbackAction(
+                label="usage",
+                data=PostbackActionData.usage.value,
+            ),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(
+                x=rich_menu_width*11/30,
+                y=rich_menu_height*1/2,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
+            ),
+            action=URIAction(
+                label="blog",
+                uri='https://cacaca-blog.vercel.app',
+            ),
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(
+                x=rich_menu_width*21/30,
+                y=rich_menu_height*1/2,
+                width=rich_menu_width*8/30,
+                height=rich_menu_height*1/2,
             ),
             action=PostbackAction(
                 label="inquiry",
@@ -77,7 +104,7 @@ raw_rich_menu = RichMenu(
 
 rich_menu_id = LINE_BOT_API.create_rich_menu(raw_rich_menu)
 
-with open("rich_menu_img/richmenu.jpg", "rb") as f:
+with open("rich_menu_imgs/richmenu.jpg", "rb") as f:
     LINE_BOT_API.set_rich_menu_image(rich_menu_id, "image/png", f)
 
 LINE_BOT_API.set_default_rich_menu(rich_menu_id)
